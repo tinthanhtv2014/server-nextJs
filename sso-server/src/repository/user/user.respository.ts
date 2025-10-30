5220c2b66404930bf1b0eb6aa29211ba7fc9fbef
tinthanhtv2014 <tinthanhtv2014@gmail.com>
2025-10-30 10:34:11 +0700

    theem UserDocument

--- sso-server/src/repository/user/user.respository.ts
+++ sso-server/src/repository/user/user.respository.ts
@@ -1,12 +1,12 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
-import { User } from "../../models/entities/user.entities";
+import { User, UserDocument } from "../../models/entities/user.entities";
 import { BaseRepository } from "../base.respository";
 
 @Injectable()
-export class UserRepository extends BaseRepository<User> {
-  constructor(@InjectModel(User.name) private readonly userModel: Model<User>) {
+export class UserRepository extends BaseRepository<UserDocument> {
+  constructor(@InjectModel("User") private readonly userModel: Model<UserDocument>) {
     super(userModel);
   }
 }
